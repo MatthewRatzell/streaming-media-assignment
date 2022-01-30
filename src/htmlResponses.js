d89f920a3d60f264fdc7cbda3fa4a005063b32e4
Matthew Ratzell <mjr4427@g.rit.edu>
2022-01-30 13:40:33 -0500

    This shit busted

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -3,11 +3,29 @@ const fs = require('fs');// pull in the file system module
 // loading some files in
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+
+const page3 = fs.readFileSync(`${__dirname}/../client/client3.html`);
+
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(index);
   response.end();
 };
 
+const getPage2 = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page2);
+  response.end();
+};
+
+const getPage3 = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page3);
+  response.end();
+};
+
 // making sure we export our newly made response functions
 module.exports.getIndex = getIndex;
+module.exports.getPage2 = getPage2;
+module.exports.getPage3 = getPage3;
--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,9 +1,92 @@
 const fs = require('fs');// pull in the file system module
 const path = require('path');
 
-const getParty = (request, response) => {
-  const file = path.resolve(__dirname, '../client/party.mp4');
+// creating local variables so we can break up this code
+let currentStart;
+let currentEnd;
+let globalStats;
 
+// function that figures out the length of the video and what part of the content is being used
+const determineBytesAndSetInfo = (request, response, contentType) => {
+  // check to see if we  got a range header if not ignore
+  let { range } = request.headers;
+  if (!range) {
+    range = 'bytes=0';
+  }
+
+  // getting our byte range for the file so we dont load more than what we need
+  const positions = range.replace(/bytes=/, '').split('-');
+
+  currentStart = parseInt(positions[0], 10);
+
+  const total = globalStats.size;
+  // check if we sent end if not assume going all the way
+
+  currentEnd = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+  if (currentStart > currentEnd) {
+    currentStart = currentEnd - 1;
+  }
+
+  // this will be how big of a chunk is being sent back in bytes
+  const chunksize = (currentEnd - currentStart) + 1;
+
+  // if everything works we want to sent back the 206 error code
+  response.writeHead(206, {
+    // how much we are sending out of the total
+    'Content-Range': `bytes ${currentStart}-${currentEnd}/${total}`,
+    // this is either bytes or none
+    'Accept-Ranges': 'bytes',
+    // how big the chunk is
+    'Content-Length': chunksize,
+    // its content type
+    'Content-Type': `${contentType}`,
+  });
+};
+
+const createStream = (response, file) => {
+  const stream = fs.createReadStream(file, { currentStart, currentEnd });
+  stream.on('open', () => {
+    stream.pipe(response);
+  });
+  stream.on('error', (streamErr) => {
+    response.end(streamErr);
+  });
+  //return stream;
+};
+
+const getResource = (request, response, filePath, contentType) => {
+  // creating our file object, does not load
+  const file = path.resolve(__dirname, `${filePath}`);
+
+  // function that checks the file and makes sure its good to go
+  fs.stat(file, (err, stats) => {
+    // taking this stats variable and putting it into something that can be accessed more easily
+    globalStats = stats;
+    if (err) {
+      if (err.cose === 'ENOENT') {
+        response.writeHead(404);
+      }
+      return response.end(err);
+    }
+
+    determineBytesAndSetInfo(request, response, contentType);
+
+    // handling our stream creation and passing it along to our const
+    const stream = createStream(response, file);
+    // returning the file
+    return stream;
+  });
+  currentEnd = null;
+  currentStart = null;
+  globalStats = null;
+};
+
+/*
+const getResource = (request, response,filePath) => {
+  // creating our file object, does not load
+  const file = path.resolve(__dirname, `${filePath}`);
+  // function that checks the file and makes sure its good to go
   fs.stat(file, (err, stats) => {
     if (err) {
       if (err.cose === 'ENOENT') {
@@ -11,28 +94,38 @@ const getParty = (request, response) => {
       }
       return response.end(err);
     }
+
+    // check to see if we  got a range header if not ignore
     let { range } = request.headers;
     if (!range) {
       range = 'bytes=0';
     }
 
+    // getting our byte range for the file so we dont load more than what we need
     const positions = range.replace(/bytes=/, '').split('-');
 
     let start = parseInt(positions[0], 10);
 
     const total = stats.size;
+    // check if we sent end if not assume going all the way
     const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
 
     if (start > end) {
       start = end - 1;
     }
 
+    // this will be how big of a chunk is being sent back in bytes
     const chunksize = (end - start) + 1;
 
+    // if everything works we want to sent back the 206 error code
     response.writeHead(206, {
+      // how much we are sending out of the total
       'Content-Range': `bytes ${start}-${end}/${total}`,
+      // this is either bytes or none
       'Accept-Ranges': 'bytes',
+      // how big the chunk is
       'Content-Length': chunksize,
+      // its content type
       'Content-Type': 'video/mp4',
     });
 
@@ -46,5 +139,5 @@ const getParty = (request, response) => {
     return stream;
   });
 };
-
-module.exports.getParty = getParty;
+*/
+module.exports.getResource = getResource;
